5312902dd18bf90b9367da9560127e88d44c0c52
venkateshprasath-glitch <venkateshprasath713@gmail.com>
2026-08-09 10:35:34 +0530

    sum of odd

--- sumofodd.py
+++ sumofodd.py
@@ -1,6 +1,6 @@
-num=input("enter a number:")
+num=int(input("enter a number:"))
 total=0
-for nin range(1,n+1):
+for n in range(1,num+1):
     if n%2!=0:
         total+=n
 print("sum of odd numbers:",total)
